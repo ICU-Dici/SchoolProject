5f0ff777e2d7ff41bea3c2635c060f910657e3f3
ICU-Dici <111076041+ICU-Dici@users.noreply.github.com>
2024-04-27 04:51:43 +0300

    Update PhysicsNavPageReducer.js

--- src/redux/PhysicsNavPageReducer.js
+++ src/redux/PhysicsNavPageReducer.js
@@ -14,9 +14,9 @@ let PhyNavPageInitState = {
     ],
     CaseContent: ['', ''],
     ParentsCases:[
-        {Name:'Великие ученые', index:1, childContent:[{name:"Великие ученые", PathTo:'/Physics/greatest-physicists'}], MainInfo: ['', ''], img:''},
-        {Name:'Поступление в ВУЗы', index:2, childContent:[{name:"Перевод баллов ЕГЭ", PathTo:'/Physics/PointsTransfer', BackgroundColor:'white'},{name:"Рейтинг ВУЗов", PathTo:'/Physics/High-Schools-rating'}], MainInfo: ['Поступление в ВУЗ', 'Заведомо спланированные действия чаще протекают по плану, ведь он есть'], img:'https://www.m24.ru/b/d/nBkSUhL2hFMgkcm0JL6BrNOp2Z3z8Zj21iDEh_fH_nKUPXuaDyXTjHou4MVO6BCVoZKf9GqVe5Q_CPawk214LyWK9G1N5ho=tb6yLu1kqkK2a9mz0Bg5Nw.jpg'},
-        {Name:'Общеобразовательные материалы', index:3, childContent:[{name:"Великие ученые", PathTo:'/Physics/greatest-physicists'},{name:"Внеклассные темы", PathTo:'/Physics/themes'}], MainInfo: ['',  ''], img:''},
+        {Name:'Великие ученые', index:1, childContent:[{name:"Великие ученые", PathTo:'/greatest-physicists'}], MainInfo: ['', ''], img:''},
+        {Name:'Поступление в ВУЗы', index:2, childContent:[{name:"Перевод баллов ЕГЭ", PathTo:'/PointsTransfer', BackgroundColor:'white'},{name:"Рейтинг ВУЗов", PathTo:'/High-Schools-rating'}], MainInfo: ['Поступление в ВУЗ', 'Заведомо спланированные действия чаще протекают по плану, ведь он есть'], img:'https://www.m24.ru/b/d/nBkSUhL2hFMgkcm0JL6BrNOp2Z3z8Zj21iDEh_fH_nKUPXuaDyXTjHou4MVO6BCVoZKf9GqVe5Q_CPawk214LyWK9G1N5ho=tb6yLu1kqkK2a9mz0Bg5Nw.jpg'},
+        {Name:'Общеобразовательные материалы', index:3, childContent:[{name:"Великие ученые", PathTo:'/greatest-physicists'},{name:"Внеклассные темы", PathTo:'/themes'}], MainInfo: ['',  ''], img:''},
     ],
     CheckAnySelection: true,
     CurrentBackgroudImg: ''
